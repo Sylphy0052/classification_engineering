cac022c74cb2cbcd88e62e75a85b48448f17dfc0
Sylphy0052 <ma17099@shibaura-it.ac.jp>
2018-06-06 14:12:25 +0900

    add: sample files

--- ubuntu/randGaussianMain.cxx
+++ ubuntu/randGaussianMain.cxx
@@ -6,11 +6,11 @@ int main(void){
   std::mt19937 mt(rnd());
   std::normal_distribution<> normDist(0.0, 1.0);
 
-  const int dimension=2, eachDataNum=50, clusterNum=2;
+  const int dimension=2, eachDataNum=50, clusterNum=3;
   const double means[clusterNum][dimension]={
-    {-1,-1},{1,1}};
+    {-1,-1},{1,1},{-1,1}};
   const double stddevs[clusterNum][dimension]={
-    {0.5,0.5},{0.5,0.5}};
+    {0.5,0.5},{0.5,0.5},{0.5,0.5}};
 
   std::cout << eachDataNum*clusterNum << "\t" << dimension << std::endl;
 
